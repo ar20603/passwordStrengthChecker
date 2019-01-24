39027cb94520612dbf71e128dfd418fe877b3199
Lavish Gulati <39210107+LavishGulati@users.noreply.github.com>
2019-01-24 18:39:42 +0530

    Final Application

---
+++ passwordStrengthChecker/WindowsApplication1/WindowsApplication1/Form2.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblSaved = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Navy
+        Me.Label2.Location = New System.Drawing.Point(29, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Username:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Navy
+        Me.Label1.Location = New System.Drawing.Point(29, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Password:"
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsername.ForeColor = System.Drawing.Color.Navy
+        Me.lblUsername.Location = New System.Drawing.Point(119, 26)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(80, 23)
+        Me.lblUsername.TabIndex = 2
+        Me.lblUsername.Text = "user.name"
+        '
+        'lblSaved
+        '
+        Me.lblSaved.AutoSize = True
+        Me.lblSaved.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSaved.ForeColor = System.Drawing.Color.Navy
+        Me.lblSaved.Location = New System.Drawing.Point(120, 56)
+        Me.lblSaved.Name = "lblSaved"
+        Me.lblSaved.Size = New System.Drawing.Size(112, 23)
+        Me.lblSaved.TabIndex = 3
+        Me.lblSaved.Text = "user.password"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(484, 161)
+        Me.Controls.Add(Me.lblSaved)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.MinimumSize = New System.Drawing.Size(500, 200)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblUsername As System.Windows.Forms.Label
+    Friend WithEvents lblSaved As System.Windows.Forms.Label
+End Class
---
+++ passwordStrengthChecker/WindowsApplication1/WindowsApplication1/Form2.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form2
+
+    Public dictSavedPasswords As New Dictionary(Of String, String)
+
+    Public Property username As String
+
+    Dim CW As Integer = Me.Width    ' Current Width of Form2
+    Dim CH As Integer = Me.Height   ' Current Height of Form2
+    Dim IW As Integer = Me.Width    ' Initial Width  of Form2
+    Dim IH As Integer = Me.Height   ' Initial Height of Form2
+
+
+    ' Function to resize the components in Form1 with respect to change in size of Form1
+
+    Private Sub Form2_Resize(ByVal sender As Object, _
+            ByVal e As System.EventArgs) Handles Me.Resize
+
+
+        Dim RW As Double = (Me.Width - CW) / CW     ' Ratio change of width
+        Dim RH As Double = (Me.Height - CH) / CH    ' Ratio change of height
+
+        ' Operations to resize the width and height
+
+        For Each Ctrl As Control In Controls
+            Ctrl.Width += CInt(Ctrl.Width * RW)
+            Ctrl.Height += CInt(Ctrl.Height * RH)
+            Ctrl.Left += CInt(Ctrl.Left * RW)
+            Ctrl.Top += CInt(Ctrl.Top * RH)
+        Next
+
+        CW = Me.Width
+        CH = Me.Height
+
+    End Sub
+
+
+    ' Function to load username and checking his/her last saved password saved in the dictionary "dictSavedPasswords"
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblUsername.Text = username
+
+        ' Checking condition if there is any last password saved for the username and changing the outout mrssage according to it
+
+        If dictSavedPasswords.ContainsKey(username) Then
+            lblSaved.Text = dictSavedPasswords(username)
+        Else
+            lblSaved.Text = "No saved password"
+        End If
+
+    End Sub
+
+End Class
